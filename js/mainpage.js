aff4518716382fa3dab88e9591bd7a9c06a0e133
Kamil Wiater <kamilwiater.uj@gmail.com>
2023-04-13 10:51:45 +0200

    JS_IMPLEMENTED

--- js/mainpage.js
+++
@@ -1,9 +0,0 @@
-const burgerIcon = document.querySelector(".burger");
-const navItemsRef = document.querySelector(".nav-items-hero");
-
-console.log(navItemsRef);
-
-burgerIcon.addEventListener("click", function () {
-  navItemsRef.classList.toggle("active");
-  burgerIcon.classList.toggle("active");
-});
